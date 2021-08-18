192eb7171ec1d7d2965c0c86035bbe9449baa50b
Eurynunez <20209380@itla.edu.do>
2021-08-18 00:21:55 -0400

    CRUD FINALIZED

---
+++ BooksMVC/Controllers/BooksController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BooksMVC.Models;
+using BooksMVC.Models.ViewModels;
+
+namespace BooksMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Book> LstBook = new List<Book>();
+            using (BOOKSDBEntities DB = new BOOKSDBEntities())
+            {
+                LstBook = (from d in DB.BOOK
+                           select new Book
+                           {
+                               Id = d.ID,
+                               Nombre = d.NOMBRE,
+                               Autor = d.AUTOR
+                           }).ToList();
+                          
+            }
+
+                return View(LstBook);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(NewBook item)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    using (BOOKSDBEntities DB = new BOOKSDBEntities())
+                    {
+                        var NewBook = new BOOK();
+                        NewBook.NOMBRE = item.Nombre;
+                        NewBook.AUTOR = item.Autor;
+                        NewBook.FECHA_CREACION = item.FechaCreacion;
+
+                        DB.BOOK.Add(NewBook);
+                        DB.SaveChanges();
+                    }
+
+                    return Redirect("~/Books");
+                }
+
+                return View(item);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        
+        public ActionResult Edit(int Id)
+        {
+            NewBook newBook = new NewBook();
+            using (BOOKSDBEntities DB = new BOOKSDBEntities())
+            {
+                var item = DB.BOOK.Find(Id);
+                newBook.Id = item.ID;
+                newBook.Nombre = item.NOMBRE;
+                newBook.Autor = item.AUTOR;
+                newBook.FechaCreacion = item.FECHA_CREACION.Value.Date;
+
+            }
+
+            return View(newBook);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(NewBook model)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    using (BOOKSDBEntities DB = new BOOKSDBEntities())
+                    {
+                        var item = DB.BOOK.Find(model.Id);
+                        item.NOMBRE = model.Nombre;
+                        item.AUTOR = model.Autor;
+                        item.FECHA_CREACION = model.FechaCreacion;
+
+                        DB.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                        DB.SaveChanges();
+                    }
+
+                    return Redirect("~/Books");
+                }
+
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            Book model = new Book();
+            using (BOOKSDBEntities DB = new BOOKSDBEntities())
+            {
+                var item = DB.BOOK.Find(Id);
+                DB.BOOK.Remove(item);
+                DB.SaveChanges();
+            }
+
+            return Redirect("~/Books");
+        }
+    }
+}
---
+++ BooksMVC/Models/ViewModels/NewBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BooksMVC.Models.ViewModels
+{
+    public class NewBook
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="NAME")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "AUTHOR")]
+        [StringLength(50)]
+        public string Autor { get; set; }
+        [Required]
+        [Display(Name = "CREATION DATE")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
